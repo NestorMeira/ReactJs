655474cb1631996309579cf103f8ed990adc216c
Natalia Uberall <nataliauberall@gmail.com>
2022-07-19 22:28:14 -0300

    hice la barra de navegación utilizando  bootstrap

---
+++ Desktop/React/proyectoreact-Meira/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NavBar from './Compnents/NavBar/NavBar';
+function App() {
+const styleApp= { padding: '10px 25px', marginTop :10}
+  return (
+    //jsx
+    <div className="container" style={styleApp}>
+      <NavBar />
+      <h1>hola</h1>
+    </div>
+  );
+}
+
+export default App;
